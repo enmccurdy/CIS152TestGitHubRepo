474e6eaaebde9c4572754fb0b513d1875f160dc5
Elizabeth McCurdy <e_mccurdy@yahoo.com>
2024-03-11 17:01:32 -0500

    Testing adding commit to CIS 152 Test Repo to push

--- CIS152TestGitHubRepo/Program.cs
+++ CIS152TestGitHubRepo/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("We are testing if GitHub Test Repo is working.");
         }
     }
 }
